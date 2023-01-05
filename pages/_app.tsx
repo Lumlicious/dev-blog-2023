b5049dc58bf1e40e96bdacacf23d87f6ab813aa7
Chad Lumley <lumlicious@Chads-MBP-55.home>
2023-01-04 19:40:04 -0600

    Added pages, sidebar layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // return
+  return (
+    <div className="flex">
+      <aside className="fixed left-0 top-0 h-screen w-60 bg-slate-700 p-10">
+        <div>Chad Lumley</div>
+        <div>Senior Front End Engineer</div>
+        <ul>
+          <li>
+            <Link href={`/`}>Blog</Link>
+          </li>
+          <li>
+            <Link href={`/projects`}>Projects</Link>
+          </li>
+          <li>
+            <Link href={`/resume`}>Resume</Link>
+          </li>
+          <li>
+            <Link href={`/about`}>About</Link>
+          </li>
+        </ul>
+      </aside>
+      <main className="ml-60 flex-1">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
---
+++ pages/resume.tsx
@@ -0,0 +1,3 @@
+export default function Resume() {
+  return <div>This is the resume page</div>;
+}
